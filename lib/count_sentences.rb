f101bbeb74c02314663f2d6f6a1369012ae2ed86
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-07-18 07:21:42 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
-
+  
   def exclamation?
-
+    self.end_with?("!")
   end
-
+  
   def count_sentences
-
+    self.split(/[.!?]/).reject {|x| x.empty?}.size
   end
+  
+  def count_sentences_words
+    self.split(/[.!?]/).map{|x| 
+    !(x.match(/\w+/).nil?)}.reject{|x| 
+    x == false}.size 
+   end
+
 end
+
+"What is this? A center for ants? 
+How can we be expected to teach children 
+to learn how to read if they can’t even 
+fit inside the building?".question?
+
+"Here's looking at you, kid.".end_with?
+
+"STELLLLLLLLLLLLLA!!!!!".end_with?
